076db1ba4f0a22399590cdd19d39bb4af46a32c5
Salman Ahmad <salman@salmanahmad.com>
2013-12-17 16:27:20 -0500

    Adding a version to the codebase.

---
+++ src/main/java/silo/lang/Version.java
@@ -0,0 +1,22 @@
+/*
+ *
+ *  Copyright 2012 by Salman Ahmad (salman@salmanahmad.com).
+ *  All rights reserved.
+ *
+ *  Permission is granted for use, copying, modification, distribution,
+ *  and distribution of modified versions of this work as long as the
+ *  above copyright notice is included.
+ *
+ */
+
+package silo.lang;
+
+public class Version {
+    public static int major = 0;
+    public static int minor = 0;
+    public static int tiny = 1;
+
+    public static String codename = "Emerald Hill";
+    public static String string = "" + major + "." + minor + "." + tiny;
+}
+
