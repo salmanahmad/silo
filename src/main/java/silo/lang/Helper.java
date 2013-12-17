cf275528db4c532f596e5c3afa98513261b11288
Salman Ahmad <salman@salmanahmad.com>
2013-12-17 16:28:39 -0500

    Removing the test cases helper class. Everything is in the lang package now.

--- src/main/java/silo/lang/Helper.java
+++ src/main/java/silo/lang/Helper.java
@@ -47,4 +47,31 @@ public class Helper {
 
         return o.toString();
     }
+
+    public static String[] getResourceListing(Class klass, String path) {
+        try {
+          URL dirURL = klass.getResource(path);
+          return new File(dirURL.toURI()).list();
+        } catch(Exception e) {
+          return null;
+        }
+    }
+
+    public static String[] getResourceListing(String path) {
+        return getResourceListing(Helper.class, path);
+    }
+
+    public static String readResource(Class klass, String path) {
+        try {
+            InputStream in = klass.getResourceAsStream(path);
+            String content = new Scanner(in).useDelimiter("\\A").next();
+            return content;
+        } catch(Exception e) {
+            return "";
+        }
+    }
+
+    public static String readResource(String path) {
+        return readResource(Helper.class, path);
+    }
 }
