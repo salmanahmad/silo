95b7f9c5a84a1845aed2bb7d2fd89cd6a8159bdb
Salman Ahmad <salman@salmanahmad.com>
2014-03-06 22:38:28 -0500

    Refactoring and implementing the rest of the file IO API.

---
+++ src/main/java/silo/io/file/writeOperation.java
@@ -0,0 +1,44 @@
+/*
+ *
+ *  Copyright 2014 by Salman Ahmad (salman@salmanahmad.com).
+ *  All rights reserved.
+ *
+ *  Permission is granted for use, copying, modification, distribution,
+ *  and distribution of modified versions of this work as long as the
+ *  above copyright notice is included.
+ *
+ */
+
+package silo.io.file;
+
+import silo.lang.Function;
+import silo.lang.ExecutionContext;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+@Function.Definition
+public class writeOperation extends Function {
+
+    @Function.Body
+    public static String invoke(ExecutionContext context, String file, String content, int position) {
+        try {
+            AsynchronousFileChannel channel = AsynchronousFileChannel.open(Paths.get(file), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+            // TODO: Becareful about the casting channel.size() to an int
+            FileOperationMessage op = new FileOperationMessage();
+            op.operationId = UUID.randomUUID().toString();
+            op.actor = context.fiber.actor;
+            op.buffer = ByteBuffer.wrap(content.getBytes());
+
+            channel.write(op.buffer, position, op, new FileOperationHandler());
+            return op.operationId;
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
