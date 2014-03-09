121f71d054654d2e9c6795b73979e70c9c099ab7
Salman Ahmad <salman@salmanahmad.com>
2014-03-08 23:17:56 -0500

    Progress on support local variables in the REPL.

---
+++ src/main/java/silo/cli/setLocal.java
@@ -0,0 +1,26 @@
+/*
+ *
+ *  Copyright 2014 by Salman Ahmad (salman@salmanahmad.com).
+ *  All rights reserved.
+ *
+ *  Permission is granted for use, copying, modification, distribution,
+ *  and distribution of modified versions of this work as long as the
+ *  above copyright notice is included.
+ *
+ */
+
+package silo.cli;
+
+import silo.lang.Function;
+import silo.lang.ExecutionContext;
+import silo.lang.Runtime;
+
+@Function.Definition
+public class setLocal extends Function {
+
+    @Function.Body
+    public static Object invoke(ExecutionContext context, Object local, Object value) {
+        Runtime runtime = context.fiber.actor.runtime;
+        return runtime.registry.put("silo.cli.shell.locals." + local.toString(), value);
+    }
+}
