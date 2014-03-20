7b65a9fd71755b6542518104f2301f7f0a515972
Salman Ahmad <salman@salmanahmad.com>
2014-03-20 00:53:12 -0400

    Updating the silo startup script to pass through JVM flags.

--- src/bin/silo.sh
+++ src/bin/silo.sh
@@ -9,7 +9,9 @@
 #
 
 
-# resolve links - $0 may be a soft-link
+
+# Determine the location of this script file.
+
 PRG="$0"
 
 while [ -h "$PRG" ] ; do
@@ -23,6 +25,40 @@ while [ -h "$PRG" ] ; do
 done
 
 
+
+# Process the command-line arguments.
+
+declare -a args
+declare -a java_args
+declare -a silo_args
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --java.cp|-j.cp|--classpath|-cp )
+            # Add the next argument to the classpath
+            CLASSPATH="$CLASSPATH:$2"
+            shift 2
+            ;;
+        --java.* )
+            java_args=("${java_args[@]}" "-${1:7}")
+            shift
+            ;;
+        -j.* )
+            java_args=("${java_args[@]}" "-${1:3}")
+            shift
+            ;;
+        * )
+            silo_args=("${silo_args[@]}" "$1")
+            shift
+            ;;
+    esac
+done
+
+
+
+# Build the classpath
+
 DIR_NAME=`dirname "$PRG"`
 # TODO - I need to switch over to the jar-with-dependencies at some point.
 # JAR_NAME="silo.jar"
@@ -31,7 +67,7 @@ DEP_PATH="$DIR_NAME/../../target/dependency/*"
 BUILD_PATH="$DIR_NAME/../../target/classes"
 #JAR_PATH="$DIR_NAME/../../target/lib/$JAR_NAME"
 
-CLASSPATH="$DEP_PATH:$BUILD_PATH:$JAR_PATH"
+CLASSPATH="$CLASSPATH:$DEP_PATH:$BUILD_PATH:$JAR_PATH"
 COMMAND_NAME="silo.lang.Main"
 
-exec java -classpath "$CLASSPATH" "$COMMAND_NAME"  "$@"
+exec java "${java_args[@]}" -classpath "$CLASSPATH" "$COMMAND_NAME" "${silo_args[@]}"
